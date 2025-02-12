da81e8cc2827894305004e15888cc0fa82ec15d5
Carl-Magnus <carl.magnus@hotmail.com>
2025-02-12 22:45:23 +0100

    Testing new acceleration
    Added calculations for new acceleration based on gravity.

--- ExtremeWindsuitRacers/GameObjects/Characters/Players/PlayerController.cs
+++ ExtremeWindsuitRacers/GameObjects/Characters/Players/PlayerController.cs
@@ -5,7 +5,7 @@ using System.Runtime.CompilerServices;
 public partial class PlayerController : CharacterBody3D
 {
     private float MaxSpeed = 8.0f;
-    private float MinSpeed = 2.0f;
+    private float MinSpeed = 0f;
     private float Acceleration = 2.5f;
     private float Deacceleration = 1.5f;
     private float CurrentSpeed = 5.0f;
@@ -13,6 +13,8 @@ public partial class PlayerController : CharacterBody3D
     private float yaw_speed = 45.0f;
     private float pitch_speed = 45.0f;
     private float roll_speed = 45.0f;
+    private float BaseAngle = 90.0f;
+    private float GravityFactor = 9.8f;
 
     private MeshInstance3D PlayerMesh;
 
@@ -34,6 +36,17 @@ public partial class PlayerController : CharacterBody3D
         Vector2 input = Input.GetVector("glide_left", "glide_right", "glide_up", "glide_down");
         float roll = Input.GetAxis("roll_left", "roll_right");
 
+        //Calculate differance between gravity normal and velocity normal. The closer velocity normal is to gravity normal, the larger % of gravity is added as acceleration.
+        //Invert by mutiplying with -1 in order to make going down increase speed.
+        //Maybe check if angle between velocity and gravity is larger than 90? If so, do new calculatation to calculate deacceleration
+        var gravityNormal = GetGravity().Normalized();
+        var velocityNormal = velocity.Normalized() * -1;
+
+        var accelerationPercent = (gravityNormal.Y - velocityNormal.Y) / gravityNormal.Y;
+        var accelerationNew = GetGravity().Y * accelerationPercent;
+        GD.Print("Acceleration: " + accelerationNew + " " + "percent of gravity: " + accelerationPercent);
+
+        //Rewrite to instead always accelerate based on angle between gravity vector and direction vector.
         if (input.Y > 0 && CurrentSpeed < MaxSpeed)
         {
             CurrentSpeed += Acceleration * (float)delta;
@@ -43,7 +56,7 @@ public partial class PlayerController : CharacterBody3D
             CurrentSpeed -= Deacceleration * (float)delta;
         }
 
-        velocity += -Basis.Z * CurrentSpeed;
+        velocity = -Basis.Z * CurrentSpeed;
         velocity += GetGravity() * (float)delta;   
 
         Velocity = velocity;
